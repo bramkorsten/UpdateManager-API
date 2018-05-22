31299a3d280529e57c408c8204337d9f2c1bab21
bramkorsten <exentory@gmail.com>
2018-05-22 13:35:07 +0200

    Added real token information from database on request of files

--- classes/download.php
+++ classes/download.php
@@ -1,19 +1,22 @@
 <?php
 
+require_once('_db.php');
+use BramKorsten\MakeItLive\DatabaseDetails as DatabaseDetails;
+require_once('connection.php');
+use BramKorsten\MakeItLive\Connection;
+
 header("Pragma: public");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Cache-Control: public");
 
 $file = NULL;
-
+$response = array();
 if (isset($_GET['file'])) {
   $file = $_GET['file'];
   if ($file == 'test.zip') {
     header('Content-Type: application/json');
-    $response = array(
-      'response' => '200 OK - Received file test.zip'
-    );
+    $response['response'] = '200 OK - Received file test.zip';
     echo(json_encode($response));
     die();
   }
@@ -22,29 +25,34 @@ if (isset($_GET['file'])) {
   exit;
 }
 
-$defaultToken = "a01e1c1af9fbe37caf4bd572eeca9fdb4bab9485";
-
-
-
+$details = new DatabaseDetails();
+$databaseData = $details->getDatabaseDetails();
+$con = new Connection();
+$db = $con->connect($databaseData);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $token = null;
 $headers = apache_request_headers();
 if(isset($headers['Authorization'])){
   $authorization = $headers['Authorization'];
   $token = str_replace('token ', "", $authorization);
-
-  if ($token === $defaultToken) {
-    $root = "../downloads/";
-    $file = $_GET['file'];
-    header("Content-Description: File Transfer");
-    header("Content-type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=\"".$file."\"");
-    header("Content-Transfer-Encoding: binary");
-    header("Content-Length: ".filesize($root.$file));
-    ob_end_flush();
-    @readfile($root.$file);
-  }
-  else {
-    header("HTTP/1.1 401 Unauthorized");
+  try {
+    $result = $db->query("SELECT * from instances WHERE `auth_token` = '{$token}'")->fetchAll(PDO::FETCH_OBJ);
+    if (!$result) {
+      header("HTTP/1.1 401 Unauthorized");
+      exit;
+    } else {
+      $root = "../downloads/";
+      $file = $_GET['file'];
+      header("Content-Description: File Transfer");
+      header("Content-type: application/octet-stream");
+      header("Content-Disposition: attachment; filename=\"".$file."\"");
+      header("Content-Transfer-Encoding: binary");
+      header("Content-Length: ".filesize($root.$file));
+      ob_end_flush();
+      @readfile($root.$file);
+    }
+  } catch (\Exception $e) {
+    header("HTTP/1.1 500 Error while checking info");
     exit;
   }
 } else {
